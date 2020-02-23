1b0b9b01d1e776837fe8ea7125cec9b9dde92b8b
Mike Pan <mike.pan@wework.com>
2020-02-23 13:40:39 +0800

    chore(dummy-vue): prettier

--- packages/jekyll-theme-dummy-vue/src/router/index.js
+++ packages/jekyll-theme-dummy-vue/src/router/index.js
@@ -1,29 +1,29 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'Home',
+    component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: require("../views/About.vue").default,
-  }
+    component: require('../views/About.vue').default,
+  },
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
