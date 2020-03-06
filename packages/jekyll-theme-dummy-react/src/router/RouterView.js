a0c2b5c7e19f7c3b402249f0a654dc3224865550
Mike Pan <mike.pan@wework.com>
2020-03-06 12:03:56 +0800

    feat(dummy-react): replace url for post

--- packages/jekyll-theme-dummy-react/src/router/RouterView.js
+++ packages/jekyll-theme-dummy-react/src/router/RouterView.js
@@ -1,9 +1,43 @@
-import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import {
+  Switch,
+  Route,
+  Redirect,
+  useHistory,
+  useLocation,
+} from 'react-router-dom';
 
 import routes from './routes';
 
 const RouterView = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  useEffect(
+    () => {
+      const urlMatch = new RegExp(/\/posts\/[^?#]+$/).exec(window.location.pathname);
+      const routeMatch = new RegExp(/\/posts\/[^?#]+$/).exec(location.pathname);
+
+      if (location.pathname === '/') {
+        if (urlMatch) {
+          // /posts/xxx#/ => /posts/xxx#/posts/xxx
+          setTimeout(() => {
+            history.replace(urlMatch[0]);
+          }, 0);
+        }
+      } else if (routeMatch) {
+        if (urlMatch) {
+          // /posts/xxx#/posts/xxx => /#/posts/xxx
+          const url = window.location.origin +
+            window.location.pathname.replace(urlMatch, '') +
+            window.location.search +
+            window.location.hash;
+          window.history.replaceState(null, null, url);
+        }
+      }
+    },
+    [location, history],
+  );
   return (
     <Switch>
       {routes.map((route) => {
