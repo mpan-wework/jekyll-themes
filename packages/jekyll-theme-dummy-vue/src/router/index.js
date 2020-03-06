deb313275065d7b60ebe5b93ed603fcbb238666e
Mike Pan <mike.pan@wework.com>
2020-03-06 12:37:31 +0800

    feat(dummy-vue): replace url for post

--- packages/jekyll-theme-dummy-vue/src/router/index.js
+++ packages/jekyll-theme-dummy-vue/src/router/index.js
@@ -10,4 +10,28 @@ const router = new VueRouter({
   routes,
 });
 
+router.beforeEach((to, _, next) => {
+  const urlMatch = new RegExp(/\/posts\/[^?#]+$/).exec(
+    window.location.pathname
+  );
+  const routeMatch = new RegExp(/\/posts\/[^?#]+$/).exec(to.path);
+  if (to.path === '/') {
+    if (urlMatch) {
+      next(urlMatch[0])
+    }
+  } else if (routeMatch) {
+    if (urlMatch) {
+      // /posts/xxx#/posts/xxx => /#/posts/xxx
+      const url =
+        window.location.origin +
+        window.location.pathname.replace(urlMatch, '') +
+        window.location.search +
+        window.location.hash;
+      window.history.replaceState(null, null, url);
+      next();
+    }
+  }
+  next();
+});
+
 export default router;
