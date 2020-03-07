dfecd44536b38168be5c68b909cfdb5416c80b7c
Mike Pan <mike.pan@wework.com>
2020-03-07 19:39:55 +0800

    feat(dummy-react): add baseurl

--- packages/jekyll-theme-dummy-react/src/index.js
+++ packages/jekyll-theme-dummy-react/src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import getBaseurl from './util/baseurl';
 
 window.REACT_APP_VERSION = process.env.REACT_APP_VERSION;
 
-Promise.resolve().then(async () => {
-  const postOuterHTML = document.getElementById('post').outerHTML;
-  const rootEl = document.getElementById('root');
-  ReactDOM.render(<App>{postOuterHTML}</App>, rootEl);
+Promise.resolve()
+  .then(getBaseurl)
+  .then(async () => {
+    const postOuterHTML = document.getElementById('post').outerHTML;
+    const rootEl = document.getElementById('root');
+    ReactDOM.render(<App>{postOuterHTML}</App>, rootEl);
 
-  // If you want your app to work offline and load faster, you can change
-  // unregister() to register() below. Note this comes with some pitfalls.
-  // Learn more about service workers: https://bit.ly/CRA-PWA
-  serviceWorker.unregister();
-});
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://bit.ly/CRA-PWA
+    serviceWorker.unregister();
+  });
