cfd2cd88a622f9b084f84a50ffb9d71067c6d1f0
Mike Pan <mike.pan@wework.com>
2020-03-09 20:08:43 +0800

    feat(dummy-react): add post navigation

--- packages/jekyll-theme-dummy-react/src/store/index.js
+++ packages/jekyll-theme-dummy-react/src/store/index.js
@@ -1,8 +1,10 @@
 import createStore from '@mpan-wework/react-store';
 import site from './site';
+import post from './post';
 
 const modules = {
   site,
+  post,
 };
 
 const { StoreProvider, useStore } = createStore(modules, true);
--- packages/jekyll-theme-dummy-react/src/views/Post.js
+++ packages/jekyll-theme-dummy-react/src/views/Post.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useStore } from '../store';
 import HtmlWrapper from '../components/html/HtmlWrapper';
 
 const Post = ({ children }) => {
-  return <HtmlWrapper html={children} />;
+  const { state, dispatch } = useStore();
+  const location = useLocation();
+  useEffect(() => {
+    if (location.state) {
+      dispatch({
+        type: 'post/download',
+        payload: {
+          post: location.state.post,
+        },
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.state]);
+
+  if (!location.state) {
+    return <HtmlWrapper html={children} />;
+  }
+
+  return <HtmlWrapper html={state.post.content} />;
 };
 
 export default Post;
