df12ea89e8c3097a6cf1bc10931906de339387cd
Mike Pan <mike.pan@wework.com>
2020-03-09 13:37:03 +0800

    feat(dummy-react): add home navigation

--- packages/jekyll-theme-dummy-react/src/components/navigation/NavSide.js
+++ packages/jekyll-theme-dummy-react/src/components/navigation/NavSide.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './NavSide.module.scss';
 
 const NavSide = () => {
-  return <div className={styles.NavSide}>NavSide</div>;
+  return (
+    <div className={styles.NavSide}>
+      <Link
+        to={{ pathname: '/' }}
+      >
+        Home
+      </Link>
+    </div>
+  );
 };
 
 export default NavSide;
